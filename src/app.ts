725678968ae1247df420b6040ba48bb4bc2ee8a7
cmjiwa <wangjinghuan@yahoo.com>
2022-06-22 17:37:39 +0300

    Intersection1-create an intersection type from two types

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
-// Code goes here!
+//define an Admin type which is an
+//object type, this is not creating
+//an object here, we are defining a type.
+//here can be done with interface as well
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+//define an Employee type
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+//create a new type based on the above
+//two types
+type ElevatedEmployee = Admin & Employee;
+
+//create an object with ElevatedEmployee type
+const e1: ElevatedEmployee = {
+  name: "rose",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
