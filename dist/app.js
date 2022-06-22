24a917878fbe72b94d836c27c2d7dd29fd8c6243
cmjiwa <wangjinghuan@yahoo.com>
2022-06-22 18:43:26 +0300

    Intersection-final 1. instersection type is In the case of object types, it's simply the combination of these object properties 2. In the case of a union type, that is basically the types they have in common.

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,9 @@
 "use strict";
 const e1 = {
-    name: "rosey",
+    name: "Rose",
     privileges: ["create-server"],
     startDate: new Date(),
 };
 console.log("e1 = ", e1);
+const aa = 3;
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -2,41 +2,45 @@
 //object type, this is not creating
 //an object here, we are defining a type.
 //here can be done with interface as well
-// type Admin = {
-//   name: string;
-//   privileges: string[];
-// };
-
-//replace above type Admin with interface Admin
-interface Admin {
+type Admin = {
   name: string;
   privileges: string[];
-}
+};
 
 //define an Employee type
-// type Employee = {
-//   name: string;
-//   startDate: Date;
-// };
-
-//replace type Empolyee with interface Employee
-interface Employee {
+type Employee = {
   name: string;
   startDate: Date;
-}
+};
 
 //create a new type based on the above
 //two types
-// type ElevatedEmployee = Admin & Employee;
-
-//replace type ElevatedEmployee with interface ElevatedEmployee
-interface ElevatedEmployee extends Admin, Employee {}
+type ElevatedEmployee = Admin & Employee;
 
 //create an object with ElevatedEmployee type
 const e1: ElevatedEmployee = {
-  name: "rosey",
+  name: "Rose",
   privileges: ["create-server"],
   startDate: new Date(),
 };
 
 console.log("e1 = ", e1);
+
+//Below is demonstrating an intersection type created from
+// 2 union types
+
+//create an union type
+type Combinable = string | number;
+
+//create an union type
+type Numeric = boolean | number;
+
+//create an intersection type from two union types
+//The Universal type now is number type,
+// because it takes the common part of the two union types.
+// Line46 and Line47 both shows error!!!
+type Universal = Combinable & Numeric;
+
+const aa: Universal = 3;
+// const bb: Universal = false;
+// const cc: Universal = "3";
